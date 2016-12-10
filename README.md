4c81af87884becc9b3aa7444808da6ed1b76ffb2
Raul <elecash@gmail.com>
2016-12-10 12:40:10 +0100

    Updated to v2.0.0 and added AOT

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ This project was generated with [angular-cli](https://github.com/angular/angular
 ## Development server
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
+## Production server
+Run `npm run aot` for a production server with AOT (ahead-of-time compilation). Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
 ## Code scaffolding
 
 Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive/pipe/service/class`.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
-import { VgCore } from "videogular2/core";
-import { VgControlsModule } from "videogular2/controls";
-import { VgOverlayPlayModule } from "videogular2/overlay-play";
-import { VgBufferingModule } from "videogular2/buffering";
-import { VgImaAdsModule } from "videogular2/ima-ads";
+import { VgCoreModule } from 'videogular2/core';
+import { VgControlsModule } from 'videogular2/controls';
+import { VgBufferingModule } from 'videogular2/buffering';
+import { VgOverlayPlayModule } from 'videogular2/overlay-play';
+import { VgSlidesModule } from 'videogular2/slides';
+import { VgStreamingModule } from 'videogular2/streaming';
+import { VgImaAdsModule } from 'videogular2/ima-ads';
 
 @NgModule({
     declarations: [
@@ -13,10 +15,12 @@ import { VgImaAdsModule } from "videogular2/ima-ads";
     ],
     imports: [
         BrowserModule,
-        VgCore,
+        VgCoreModule,
         VgControlsModule,
-        VgOverlayPlayModule,
         VgBufferingModule,
+        VgOverlayPlayModule,
+        VgSlidesModule,
+        VgStreamingModule,
         VgImaAdsModule
     ],
     providers: [],
