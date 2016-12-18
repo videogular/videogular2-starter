c6a299f2363f7507ed5d54c8554b3c4e0cf89e5a
Raul <elecash@gmail.com>
2016-12-18 11:06:09 +0100

    Update to videogular 3.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { VgCoreModule } from 'videogular2/core';
 import { VgControlsModule } from 'videogular2/controls';
 import { VgBufferingModule } from 'videogular2/buffering';
 import { VgOverlayPlayModule } from 'videogular2/overlay-play';
-import { VgSlidesModule } from 'videogular2/slides';
 import { VgStreamingModule } from 'videogular2/streaming';
 import { VgImaAdsModule } from 'videogular2/ima-ads';
 
@@ -19,7 +18,6 @@ import { VgImaAdsModule } from 'videogular2/ima-ads';
         VgControlsModule,
         VgBufferingModule,
         VgOverlayPlayModule,
-        VgSlidesModule,
         VgStreamingModule,
         VgImaAdsModule
     ],
